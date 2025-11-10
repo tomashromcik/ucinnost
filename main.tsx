5a68c8c0206a7cc3c07f38b9d4da1f6fbc984800
tomashromcik <tomas.hromcik@zsbohuslavice.cz>
2025-11-10 15:43:56 +0100

    Update main.tsx

--- main.tsx
+++ main.tsx
@@ -1,4 +1,4 @@
-```ts
+ts
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
@@ -10,4 +10,4 @@ createRoot(document.getElementById('root')!).render(
 <App />
 </React.StrictMode>
 )
-```
+
