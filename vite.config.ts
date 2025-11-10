1cd2214edc7c3901d069799999c3787111fb34b5
tomashromcik <tomas.hromcik@zsbohuslavice.cz>
2025-11-10 15:41:55 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-```ts
+ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -7,4 +7,4 @@ export default defineConfig({
 plugins: [react()],
 // `base` nastaví workflow při buildu, takže tady výchozí hodnotu necháme prázdnou
 })
-```
+
